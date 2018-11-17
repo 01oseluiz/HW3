7ab6e08ce19622fda859b7b9a0abbf600dbdf570
01oseluiz <joseluizgnogueira@live.com>
2018-11-17 10:08:38 -0200

    add director spec tests

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,10 +1,42 @@
-require 'rspec'
 require 'rails_helper'
 
-describe 'My behaviour' do
+RSpec.describe MoviesController, type: :controller do
 
-  it 'should do something' do
+  describe 'Add director and show all movies with the same director' do
 
-    true.should == false
+    context 'when movie already exist' do
+      before do
+        Movie.create(title: 'The Terminator',  rating: 'PG-13', release_date: '26-Oct-1984')
+        Movie.create(title: 'When Harry Met Sally',  rating: 'R', release_date: '21-Jul-1989')
+      end
+
+      it 'should add director to an existent movie' do
+        expect(Movie.count).to_not eq(0)
+
+        Movie.all.each do |movie|
+          @movie_params = movie.attributes
+          @movie_params[:director] = "Stan Lee"
+
+          post :update, params: {id:movie.id, movie: @movie_params}
+
+          expect(Movie.find(movie).director).to eq("Stan Lee")
+          expect(response).to redirect_to(movie_path(movie))
+        end
+      end
+
+      context 'when director exist' do
+        it 'should show all movies with the same director' do
+          pending
+        end
+      end
+
+      context 'when director not exist' do
+        it 'should not show all movies with the same director' do
+          pending
+        end
+      end
+    end
+
   end
+
 end
--- spec/models/movies_spec.rb
+++ spec/models/movies_spec.rb
@@ -1,11 +1,43 @@
-require 'rspec'
 require 'rails_helper'
 
 
-describe 'My behaviour' do
+RSpec.describe Movie, type: :model do
 
-  it 'should do something' do
+  describe 'Add director to movie and find with the same director' do
 
-    true.should == false
+    context 'when movie already exist' do
+      before do
+        Movie.create(title: 'The Terminator',  rating: 'PG-13', release_date: '26-Oct-1984')
+        Movie.create(title: 'When Harry Met Sally',  rating: 'R', release_date: '21-Jul-1989')
+      end
+
+      it 'should add a director to an movie' do
+
+        expect(Movie.count).not_to eq(0)
+
+        Movie.all.each do |movie|
+          movie.director = "Stan Lee"
+          movie.save
+        end
+
+        Movie.all.each do |movie|
+          expect(movie.director).to eq("Stan Lee")
+        end
+      end
+
+      context 'when director exist' do
+        it 'should find movies with the same director' do
+          pending
+        end
+      end
+
+      context 'when director not exist' do
+        it 'should not find movies with the same director' do
+          pending
+        end
+      end
+    end
+
   end
+
 end
